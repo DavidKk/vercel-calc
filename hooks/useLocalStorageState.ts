4c7a3e860bce03cb5e360d3b534b188d2761762c
DavidJones <qowera@gmail.com>
2025-10-05 12:35:58 +0800

    feat: add formula

--- hooks/useLocalStorageState.ts
+++ hooks/useLocalStorageState.ts
@@ -2,13 +2,15 @@ import { useEffect, useState } from 'react'
 
 export function useLocalStorageState<T>(key: string, defaultValue: T | (() => T)): [T, React.Dispatch<React.SetStateAction<T>>] {
   const token = `LOCAL_STORAGE_STATE_TOKEN_${key}`
-  const [state, setState] = useState<T>(() => {
-    if (localStorage.getItem(token)) {
-      return JSON.parse(localStorage.getItem(token)!)
-    }
+  const [state, setState] = useState<T>(() => (typeof defaultValue === 'function' ? (defaultValue as () => T)() : defaultValue))
 
-    return typeof defaultValue === 'function' ? (defaultValue as () => T)() : defaultValue
-  })
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const content = localStorage.getItem(token)
+      const data = content ? JSON.parse(content) : null
+      setState(data)
+    }
+  }, [])
 
   useEffect(() => {
     try {
